21d4b3a1fafac2356cf0fce0e9ac642992b973ce
Thulitha Oshadha <thulitha@webmotech.com>
2024-08-06 22:35:40 +0530

    refactor configurtion

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AllConfigType } from './config/config.type';
 import { useContainer } from 'class-validator';
 import { ValidationPipe } from '@nestjs/common';
 import validationOptions from './utils/validation-options';
+import { ResponseInterceptor } from './utils/common/response.interceptor';
 
 
 async function bootstrap() {
@@ -15,6 +16,9 @@ async function bootstrap() {
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe(validationOptions));
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(configService.getOrThrow('app.port', { infer: true }));
   //await app.listen(4000)
 }
